3fc35593de7e2e028f14408889d512334811b738
24LP7490 24LP7490 <itsmebuya@gmail.com>
2024-12-03 18:05:23 +0800

    bodlogonuud

---
+++ index.js
@@ -0,0 +1,121 @@
+//exercise 1 
+
+// const a1 = Number(prompt("enter your number"))
+
+// console.log(a1)
+
+// if (Number.isNaN(a1)) {
+//     console.log("NaN")
+//     console.log("Please enter number");
+// }
+// else if (a1 % 3 === 0) {
+//     console.log("its Fizz")
+// }
+// else if (a1 % 5 === 0) {
+//     console.log("its Buzz")
+// }
+// else if (a1 % 3 !== 0 || a1 % 5 !== 0) {
+//     console.log("given input value")
+// }else{
+//     console.log("else here");
+// }
+
+// exer 2
+
+// const b1 = prompt("enter first number");
+// const b2 = prompt("enter second number");
+// const b3 = prompt("enter third number");
+// const b4 = prompt("enter fourth number");
+// const b5 = prompt("enter fifth number");
+// console.log(b1 + " "+b2+" "+b3 +" "+b4 +" "+b5)
+
+// if(b1 >= b2 && b1>= b3 && b1>=b4 && b1>=b5){
+//     console.log(b1+ " ene too n ih")
+// } else if (b2 >= b1 && b2>= b3 && b2>=b4 && b2>=b5){
+//     console.log(b2 + "ene too n ihee")
+// } else if (b3 >= b2 && b3>= b1 && b3>=b4 && b3>=b5){
+//     console.log(b3 + " too n ih")
+// } else if (b4 >= b2 && b4>= b3 && b4>=b1 && b4>=b5){
+//     console.log("ih too n " +b4)
+// }
+// else( console.log("suulin too n ih baina "+ b5 ))
+
+//exer 3
+
+// const c1 = "a"
+// const c2 = 6
+
+// if(typeof c1 !==  "number" || typeof c2 !==  "number") { 
+//     console.log("invalid input")
+// } else{
+//     if(c1 >= c2) {
+//         console.log("ehnii too ih")
+//     }
+//     else {
+//         console.log("2doh too n ih")
+//     }
+// }
+
+//4 
+// const d1 = 0
+// const d2 = 6
+// const d3 = -2
+// const d4 = d1 + d2 + d3
+// if(d4 >0) {
+//     console.log ("")
+// }
+// else (
+//     console.log("-")
+// )
+
+//5
+
+// let x =1
+// let y = 3
+// if(x>y) {
+//     console.log(true)
+// }
+// else if(x==y) {
+//     console.log(false)
+// }
+// else if (x!==y) {
+//     console.log("not sure")
+// }
+
+//6 
+// dun = 100;
+// if(100>= dun && dun >=0) {
+//     if( dun >=90) {
+//         console.log("A")
+//     }
+//     else if(dun >=80){
+//         console.log("B")
+//     }
+//     else if(dun >=70){
+//         console.log("C")
+//     }
+//     else if (dun >=60){
+//         console.log("D")
+//     }
+//     else {console.log("F")}
+// }
+
+//7
+// let crewStatus = true
+
+// if (crewStatus) {
+//   console.log("Crew Ready");
+// } else {
+//   console.log("Crew Not Ready");
+// }
+
+// let computerStatusCode = 200
+
+// if (computerStatusCode === 200) {
+//     console.log("Computer is rebooting.");
+// } else if (computerStatusCode === 400) {
+//     console.log("Success!");
+// } else {
+//     console.log("Computer offline!");
+// }
+
